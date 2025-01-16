2db0467bb02ad4eb91e1b398fb4793d18bb10c9e
haley227 <haleysmith227@gmail.com>
2025-01-16 15:41:39 -0700

    Improve nesting

--- src/lib/live-preview.js
+++ src/lib/live-preview.js
@@ -6,7 +6,9 @@ ContentfulLivePreview.init({ locale: 'en-US', enableLiveUpdates: true, debugMode
 const updateEntry = (entry, value) => {
   console.log('updateEntry', entry, value);
   if (typeof value === 'string') {
+    console.log('value', value);
     if (entry.nodeName === 'A') {
+      console.log('entry.nodeName', entry.nodeName);
       entry.href = value;
     } else {
       entry.textContent = value;
@@ -27,7 +29,7 @@ const callback = (data) => {
 
       if (value.hasOwnProperty('sys')) {
         console.log('object value', value);
-        for (const [subKey, subValue] of Object.entries(value)) {
+        for (const [subKey, subValue] of Object.entries(value.items)) {
           console.log('subKey', subKey, subValue);
           const subEntry = document.querySelector(`[data-contentful-entry-id="${value.sys.id}"][data-contentful-field-id="${subKey}"]`);
           if (subEntry) updateEntry(subEntry, subValue);
